eb1f8b9df7247a618d5127389b9e5a3a82ff689c
kkaushik14 <147918121+kkaushik14@users.noreply.github.com>
2024-06-24 18:15:23 +0000

    conversion & operations

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,71 @@
+let score = 45
+// let score = "25"  //This is string and change to number after conversion and value is "Number"
+// let score = "25deft"  //This is string and change to number after conversion and value is "NaN"
+//let score = null //This is consider as object as value is "0" 
+// let score = "kaushik"  //This is string and change to number after conversion and value is "NaN"
+// let score = undefined  //This is undefined and change to number after conversion and value is "NaN"
+// let score = true  //This is boolean and change to number after conversion and value is "1"
+
+// console.log(typeof score);
+// console.log(typeof (score));//both syntax are same
+
+// let valueINNumber = Number(score)
+// console.log(typeof valueINNumber);  //NaN
+// console.log(valueINNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let Number = 25
+
+
+let stringNumber = String(Number)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+//summary
+
+// 45 :- existing Number
+// "25ada" :- NaN
+// boolean= "":-false  ;  "kaushik":- true
+
+//***********************************Operation************************************
+
+let values = 5
+let negValues = -values
+
+// console.log(negValues)
+// console.log(5+5)
+// console.log(6-9)
+// console.log(6*9)
+// console.log(6/9)
+// console.log(6**9)
+// console.log(6%9)
+
+
+let str1 = "Hello"
+let str2 = " Kaushik"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(+true)  //not recommended
+// console.log(+"")  //not recommended
+
+let counting = 20
+// counting++; //postfix
+// ++counting; //prefix
+
+console.log(counting);
