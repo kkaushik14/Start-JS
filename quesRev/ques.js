a088c6b2de16820a0dcd600cf23fa159c81f9df7
Kumar Kaushik <kumarkaushik@Kumars-MacBook-Pro.local>
2025-02-17 21:55:08 +0530

    Prototype Chaining

--- quesRev/ques.js
+++ quesRev/ques.js
@@ -310,4 +310,29 @@ function outer() {
     }
     return inner();
 }
-console.log(outer());
+// console.log(outer());
+
+
+
+// Prototypes in JavaScript
+// Task: Prototype Chaining
+// Create a constructor function Animal that has a method speak() that return 'Animal speaking'.
+// Then create another constructor Dog that inherits from Animal using prototypes.
+// The Dog constructor should add a method bark() that returns 'Woof!'. Demonstrate the prototype chain between Dog and Animal.
+function Animal() {}
+Animal.prototype.speak = function() {
+    return `Animal speaking...`;
+}
+
+function Dog() {}
+Dog.prototype = Object.create(Animal.prototype);
+
+Dog.prototype.bark = function() {
+    return `Woof!`;
+};
+
+Dog.prototype.constructor = Dog;
+let speak = new Animal().speak();
+let bark = new Dog().bark();
+console.log(speak);
+console.log(bark);
